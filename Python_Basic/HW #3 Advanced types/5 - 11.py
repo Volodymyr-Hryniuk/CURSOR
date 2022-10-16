e6f7d12f98db81d5c98fdfa1f601f63a09c20f42
Volodymyr-Hryniuk <wourendeoreali@gmail.com>
2022-10-16 19:46:08 +0300

    5 - 11 Part HW is Done

---
+++ Python_Basic/HW #3 Advanced types/5 - 11.py
@@ -0,0 +1,42 @@
+# String formatting:
+
+# Replace the placeholders with a value:
+# "Anna has ___ apples and ___ peaches."
+
+# 5. With .format and curly braces {}
+
+print("5.")
+print("Anna has {} apples and {} peaches.".format(16, 8), "\n")
+
+# 6. By passing index numbers into the curly braces.
+
+print("6.")
+print("Anna has {0} apples and {1} peaches.".format(10, 11), "\n")
+
+# 7. By using keyword arguments into the curly braces.
+
+print("7.")
+print("Anna has {apples} apples and {peaches} peaches.".format(apples=5, peaches=7), "\n")
+
+# 8. With indicators of field size (5 chars for the first and 3 for the second).
+
+print("8.")
+print("Anna has {0:2} apples and {1:3} peaches." .format(6, 8), "\n")
+
+# 9. With f-strings and variables.
+
+print("9.")
+apples = 3
+peaches = 7
+print(f"Anna has {apples} apples and {peaches} peaches.", "\n")
+
+# 10. With % operator
+
+print("10.")
+print("Anna has %s apples and %s peaches." % (apples, peaches), "\n")
+
+# 11. With variable substitutions by name (hint: by using dict)
+
+print("11.")
+count_fruit = {'appl': 7, 'peach': 4}
+print(f"Anna has {count_fruit['appl']} apples and {count_fruit['peach']} peaches.")
