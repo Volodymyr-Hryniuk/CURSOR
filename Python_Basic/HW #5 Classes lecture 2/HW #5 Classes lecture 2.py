94d00f04494a011576edcb289bba33a14886da5e
Volodymyr-Hryniuk <wourendeoreali@gmail.com>
2022-10-24 22:26:06 +0300

    HW #5 Work in progress...

--- Python_Basic/HW #5 Classes lecture 2/HW #5 Classes lecture 2.py
+++ Python_Basic/HW #5 Classes lecture 2/HW #5 Classes lecture 2.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 # 1.
 print("#1")
 
@@ -115,12 +117,26 @@ class Concert:
 print("\n#6")
 
 
+@dataclasses.dataclass
 class AddressBookDataClass:
     """
     Create dataclass with 7 fields - key (int), name (str), phone_number (str),
                                      address (str), email (str), birthday (str), age (int)
     """
+    key: int
+    name: str
+    phone_number: str
+    address: str
+    email: str
+    birthday: str
+    age: int
 
+
+address_book = AddressBookDataClass(1, 'ivan', '095xxxxxxx', 'Ozerna str.', 'xxx@ukr.net', '6 December', 23)
+address_book_1 = AddressBookDataClass(1, 'ihor', '096xxxxxxx', 'Bolotna str.', '1xx@ukr.net', '25 November', 25)
+
+print(address_book)
+print(address_book_1)
 
 # 7. Create the same class (6) but using NamedTuple
 print("\n#7")
