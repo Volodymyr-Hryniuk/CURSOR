c22219385bd18e459c2c681e548839a24c892b1e
Volodymyr-Hryniuk <wourendeoreali@gmail.com>
2022-11-02 21:43:52 +0200

    HW #6 Work is Done...

--- Python_Basic/HW #6 Advanced classes/HW #6 Advanced classes.py
+++ Python_Basic/HW #6 Advanced classes/HW #6 Advanced classes.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod, ABC
+
 # 1. Implement class iterator for Fibonacci numbers https://en.wikipedia.org/wiki/Fibonacci_number
 # Iterator get numbers of first Fibonacci numbers
 # Example:
@@ -77,8 +79,8 @@ class FibonacciGenerator:
             self.count -= 1
 
 
-iterat = FibonacciGenerator(10)
-for i in iterat.fibonacci_generator():
+iteration = FibonacciGenerator(10)
+for i in iteration.fibonacci_generator():
     print(i)
 
 # 3. Write generator expression that returns square numbers of integers from 0 to 10
@@ -95,9 +97,96 @@ for i in square(10):
 
 # 4. Create an interface for the Laptop with the next methods: Screen, Keyboard, Touchpad, WebCam, Ports, Dynamics
 # and create an HPLaptop class by using your interface.
-print("\n#4")
+
+# print("\n#4")
+
+
+class Laptop (ABC):
+
+    @abstractmethod
+    def screen(self):
+        print('Hello World')
+
+    @abstractmethod
+    def keyboard(self):
+        print('Input device')
+
+    @abstractmethod
+    def touchpad(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def webcam(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def ports(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def dynamics(self):
+        raise NotImplementedError
+
+
+class HPLaptop(Laptop):
+
+    def __init__(self):
+        self.parameters = {}
+
+    def screen(self):
+        return 'Hello World'
+
+    def keyboard(self):
+        print('Input device')
+
+    def touchpad(self):
+        print('Touch input device')
+
+    def webcam(self):
+        print('Video recording Device')
+
+    def ports(self):
+        print('Devices are connected using ports')
+
+    def dynamics(self):
+        print('Sound output device')
+
 
 # 5. Create an abstract class for the Car with the next methods: drive, stop, open_door, close_door, turn_on_light,
 # turn_off_light, enable_radio, disable_radio, where drive and stop will be predefined with some realization, all others
 # should be abstract.
-print("\n#5")
+
+# print("\n#5")
+
+
+class Car (ABC):
+
+    def drive(self):
+        print('Drive')
+
+    def stop(self):
+        print('Stop')
+
+    @abstractmethod
+    def open_door(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def close_door(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def turn_on_light(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def turn_off_light(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def enable_radio(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def disable_radio(self):
+        raise NotImplementedError
